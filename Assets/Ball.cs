0a73e08bce1f56737a17810fa21ac9e4ed6945ec
Benz <ben.afr@gmail.com>
2023-06-02 16:46:49 +0300

    upd : balls files structure

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,70 +1,36 @@
 using UnityEngine;
 
-public class BallBehavior : MonoBehaviour
+public abstract class Ball : MonoBehaviour
 {
-    public float maxVerticalForce = 15f;
-    public float maxHorizontalFactor = 0.5f;
-    // public BallType ballType;
-    public int constantDirection = 1;
-    private Rigidbody2D rb;
-    [SerializeField]private Vector2 initialVelocity;
+    #region Editor Components
 
-    private void Awake()
+    [SerializeField] private Rigidbody2D _rigidBody;
+    [SerializeField] private Vector2 _initialVelocity;
+    [SerializeField] private float _maxVerticalForce = 15f;
+    [SerializeField] private float _maxHorizontalFactor = 0.5f;
+    
+    [SerializeField] protected Transform _transform;
+
+    #endregion
+
+    #region Fields
+
+    protected IBallMovementController _ballMovementController;
+
+    #endregion
+
+    #region Methods
+
+    private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _ballMovementController = new BallMovementController();
+        _ballMovementController.InitializeMovement(_transform, _rigidBody, _initialVelocity, _maxVerticalForce, _maxHorizontalFactor);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        var direction = GetCollisionDirection(collision);
-        if (direction.x != 0)
-        {
-            initialVelocity = new Vector2(direction.x, initialVelocity.y);
-        }
-        
-        float currentHeight = transform.position.y;
-        float maxHeight = 3; // ballType.maxHeight;
-        
-        if (currentHeight <= maxHeight)
-        {
-            float verticalForce = Mathf.Lerp(0f, maxVerticalForce, 1) * direction.y;
-        
-            float horizontalForce = Mathf.Lerp(-maxHorizontalFactor, maxHorizontalFactor, 1) * direction.x;
-        
-            Vector2 jumpForce = new Vector2(horizontalForce, verticalForce);
-            rb.AddForce(jumpForce, ForceMode2D.Impulse);
-
-        }
+        _ballMovementController.AdjustMovementOnCollision(collision);
     }
 
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(initialVelocity.x, rb.velocity.y);
-    }
-    
-    private Vector2 GetCollisionDirection(Collision2D collision)
-    {
-        var direction = Vector2.zero;
-
-        foreach (ContactPoint2D contact in collision.contacts)
-        {
-            Vector2 contactNormal = contact.normal;
-
-            direction.x = contactNormal.x switch
-            {
-                > 0 => 1,
-                < 0 => -1,
-                _ => direction.x
-            };
-
-            direction.y = contactNormal.y switch
-            {
-                > 0 => 1,
-                < 0 => -1,
-                _ => direction.y
-            };
-        }
-
-        return direction;
-    }
+    #endregion
 }
---
+++ Assets/BasicBall.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BasicBall : Ball
+{
+    #region Editor Components
+
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    
+    #endregion
+    
+    #region Methods
+    
+    public void SetBallVisuals(Sprite sprite, Vector2 localScale)
+    {
+        _spriteRenderer.sprite = sprite;
+        _transform.localScale = localScale;
+    }
+
+    #endregion
+}
